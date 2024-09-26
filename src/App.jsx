4e2fcf5ac06430063ffdba094fc72b5a3d67eff3
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-09-26 23:40:32 +0545

    video Caresoul Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Hero } from "./components/Hero";
-import { Highlights } from "./components/Highlights";
+import Highlights from "./components/Highlights";
 import { Navbar } from "./components/Navbar";
 
 function App() {
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,14 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React from "react";
 import { rightImg, watchImg } from "../utils";
+
 import VideoCarousel from "./VideoCarousel";
 
-export const Highlights = () => {
+const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
     gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
-  });
+  }, []);
+
   return (
     <section
       id="highlights"
@@ -19,19 +20,23 @@ export const Highlights = () => {
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
+
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
               Watch the film
-              <img src={watchImg} className="ml-2" alt="watch" />
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
             <p className="link">
               Watch the event
-              <img src={rightImg} className="ml-2" alt="watch" />
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
         </div>
+
         <VideoCarousel />
       </div>
     </section>
   );
 };
+
+export default Highlights;
